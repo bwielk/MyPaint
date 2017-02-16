4d898fdf0895694bcb84b58ce2d00f170efa57f5
Blazej <bwielk@gmail.com>
2017-02-16 13:16:02 +0000

    Improvements

--- public/index.js
+++ public/index.js
@@ -26,16 +26,112 @@ var app = function(){
 
 
 ///////////////////////////////////////DOGE////////////////////////////////////////////
-  var img = document.createElement('img');
-  img.src = 'http://emojis.slackmojis.com/emojis/images/1457563042/312/doge.png';
+  // var img = document.createElement('img');
+  // img.src = 'http://cultofthepartyparrot.com/parrots/witnessprotectionparrot.gif';
 
-  var drawDoge = function(event){
-    console.log('img',img)
-    context.drawImage(img, event.x, event.y, 90, 90);
-  }
+  // var drawDoge = function(event){
+  //   console.log('img',img)
+  //   context.drawImage(img, event.x, event.y, 90, 90);
+  // }
 
-  canvas.onclick = drawDoge
+  // canvas.onclick = drawDoge
+/////////////////////////////////////////RANDOM2/////////////////////////////////////
 
+
+var changeColor = function(){
+  context.strokeStyle = this.value;
+  context.fillStyle = this.value;
+}
+
+var colorPicker = document.querySelector('#input-color');
+colorPicker.onchange = changeColor;
+var drawAnything = function(x,y){
+  var xpar = x;
+  var ypar = y;
+  context.ellipse(xpar, ypar, xpar-=Math.floor((Math.random() * 100) + 1)
+, ypar+=Math.floor((Math.random() * 40) + 1)
+, 45 * Math.PI/180, 0, 2 * Math.PI);
+  context.stroke();
+  context.fill();
+}
+
+canvas.onclick = function(event){
+  drawAnything(event.x, event.y);
+}
+
+///////////////////////////////////////RANDOM////////////////////////////////////////
+
+
+// var changeColor = function(){
+//   context.strokeStyle = this.value;
+//   context.fillStyle = this.value;
+// }
+
+// var colorPicker = document.querySelector('#input-color');
+// colorPicker.onchange = changeColor;
+// var drawAnything = function(x,y){
+//   var xpar = x;
+//   var ypar = y;
+//   context.ellipse(xpar, ypar, xpar-=Math.floor((Math.random() * 100) + 1)
+// , ypar+=Math.floor((Math.random() * 40) + 1)
+// , 45 * Math.PI/180, 0, 2 * Math.PI);
+//   context.stroke();
+//   context.fill();
+// }
+
+// canvas.onclick = function(event){
+//   drawAnything(event.x, event.y);
+// }
+
+/////////////////////////////////////SQUARE/////////////////////////////////////////
+
+// var changeColor = function(){
+//   context.strokeStyle = this.value;
+//   context.fillStyle = this.value;
+// }
+
+// var colorPicker  = document.querySelector('#input-color');
+// colorPicker.onchange = changeColor;
+
+// var drawSquare = function(x, y){
+//   var xpar = x;
+//   var ypar = y;
+//   context.strokeRect(xpar, ypar, xpar + 3, ypar + 5);
+//   context.fill();
+//   context.stroke();
+  
+// }
+
+// canvas.onclick = function(event){
+//   drawSquare(event.x, event.y);
+// }
+
+////////////////////////////////////TRIANGLE////////////////////////////////////////
+
+
+// var changeColor = function(){
+//     context.strokeStyle = this.value;
+//     context.fillStyle = this.value;
+//   }
+
+// var colorPicker  = document.querySelector('#input-color');
+// colorPicker.onchange = changeColor;
+
+// var drawTriangle = function(x, y){
+//   var xpar = x;
+//   var ypar = y;
+//   context.beginPath();
+//   context.moveTo(xpar,ypar);
+//   context.lineTo(xpar+10,ypar+50);
+//   context.lineTo(xpar+60,ypar+20);
+//   context.closePath();
+//   context.stroke();
+//   context.fill();
+// }
+
+// canvas.onclick = function(event){
+//   drawTriangle(event.x, event.y);
+// }
 ///////////////////////////////////////CIRCLE///////////////////////////////////////
   // var changeColor = function(){
   //   context.strokeStyle = this.value;
